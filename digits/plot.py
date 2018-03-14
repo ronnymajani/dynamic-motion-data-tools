81f36e538bfb5041533c7d1ce62d416b6be6ff1b
Ronny Majani <ronnymajani@gmail.com>
2018-03-14 21:27:52 +0300

    Improved Plotting function
    - added ability to set window title with digit's label
    - added color map to show the flow of points with time
    - made the line color constant
    - added constants

--- digits/plot.py
+++ digits/plot.py
@@ -7,7 +7,7 @@ from scipy import interpolate, ndimage
 
 
 
-def show_digit(digit, show_points=True, show_lines=True, padding=100):
+def show_digit(digit, label=None, show_points=True, show_lines=True, padding=100):
     """ Displays the given digit
     @param digit: A sequence of [X,Y,P,t] points that represent a dynamically drawn handwritten digit
     """
@@ -16,6 +16,7 @@ def show_digit(digit, show_points=True, show_lines=True, padding=100):
     x, y, p, t = np.split(data, 4, axis=1)
     x = x.flatten()
     y = y.flatten()
+    
     # Delete identical points to not get an error from the spline function
     # https://stackoverflow.com/questions/47948453/scipy-interpolate-splprep-error-invalid-inputs/47949170#47949170
     okay = np.where(np.abs(np.diff(x)) + np.abs(np.diff(y)) > 0)
@@ -30,25 +31,35 @@ def show_digit(digit, show_points=True, show_lines=True, padding=100):
     # Generate B-Spline
     tck, u = interpolate.splprep([xn, yn], s=0)
     xi, yi = interpolate.splev(np.linspace(0, 1, 1000), tck)
+    
     # plot
-    # invert Y axis (our coordinates assume X, Y is the top left corner)
     ax = plt.gca()
+    # Set the Y and X axis limits
+    # we invert the Y axis (our coordinates assume X, Y is the top left corner)
     y_max = max(y.max(), yi.max()) + padding
     y_min = min(y.min(), yi.min()) - padding
     x_max = max(x.max(), xi.max()) + padding
     x_min = min(x.min(), xi.min()) - padding
     ax.set_xlim(x_min, x_max)
     ax.set_ylim(y_max, y_min)
+    # Preserve the aspect ratio and scale between the X and Y axis
     ax.set_aspect('equal', adjustable='box')
+    # Set the X axis to be drawn on the top
     ax.xaxis.tick_top()
     ax.xaxis.set_label_position('top') 
+    # Set axes labels
     plt.xlabel("X")
     plt.ylabel("Y")
-    
+    # Set label as title
+    if label is not None:
+        plt.gcf().canvas.set_window_title("Digit: %d" % label)
+    # Show the points of the digit
     if show_points:
-        plt.plot(x, y, 'o')
+        c = np.arange(len(x))
+        plt.scatter(x, y, c=c, cmap="inferno")
+    # Interpolate between points and draw a connecting line (spline)
     if show_lines:
-        plt.plot(xi, yi, '-')
+        plt.plot(xi, yi, '-', c="#1f77b4ff")
 
 
 
