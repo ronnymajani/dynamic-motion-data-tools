33ec7b41b9dae8d0d8cca2361f565da17409417c
Ronny Majani <ronnymajani@gmail.com>
2018-03-14 16:11:24 +0300

    Added preprocessing module

---
+++ digits/preprocessing.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+def apply_mean_centering(digit, inplace=False):
+    """ Translates the coordinates X, Y so their mean is aligned with the origin (0,0)
+    @param digit: The digit to apply the transformationt to
+    @param inplace: If True, the operation is performed inplace
+                    (this only works if digit is already a numpy array)
+                    If False, a copy of the given data is made
+    @returns The resulting digit as a copy if inplace is False
+    @returns The given digit itself after applying the transformation
+    """
+    if not isinstance(digit, np.ndarray) or inplace == False:
+        digit = np.array(digit)
+        
+    x = digit[:, 0]
+    y = digit[:, 1]
+    
+    mean_x, mean_y = x.mean(), y.mean()
+    
+    digit[:, 0] = x - mean_x
+    digit[:, 1] = y - mean_y
+    
+    return digit
+
+
+def apply_unit_distance_normalization(digit, inplace=False):
+    """ Scales the coordinates (using minMax scaling) so the maximum euclidian distance to any of the points
+    in the given digit is set to 1.0
+    @param digit: The digit to apply the transformationt to
+    @param inplace: If True, the operation is performed inplace
+                    (this only works if digit is already a numpy array)
+                    If False, a copy of the given data is made
+    @returns The resulting digit as a copy if inplace is False
+    @returns The given digit itself after applying the transformation
+    """
+    if not isinstance(digit, np.ndarray) or inplace == False:
+        digit = np.array(digit)
+        
+    x = digit[:, 0]
+    y = digit[:, 1]
+    
+    mean_x, mean_y = x.mean(), y.mean()
+    
+    squared_euclidian_distance = np.square(x - mean_x) + np.square(y - mean_y)
+    max_distance = squared_euclidian_distance.max()
+    scale = np.sqrt(max_distance)
+    
+    digit[:, 0] = x / scale
+    digit[:, 1] = y / scale
+    return digit
+
+
+
+
+
+
+
+
+
+
