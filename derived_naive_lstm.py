44cc455373728baac73212141b0e33cb8d87f7fb
Ronny Majani <ronnymajani@gmail.com>
2018-04-07 15:50:32 +0300

    Added tensorboard callback

--- derived_naive_lstm.py
+++ derived_naive_lstm.py
@@ -28,30 +28,37 @@ X_train_valid, X_test, Y_train_valid, Y_test = train_test_split(data, labels, sh
 X_train, X_valid, Y_train, Y_valid = train_test_split(X_train_valid, Y_train_valid, shuffle=True, stratify=Y_train_valid, random_state=42)
 
 #%%
-
+from keras import backend as K
+K.set_learning_phase(1) #set learning phase
 from keras import Sequential
 from keras.layers import Masking
 from keras.layers import LSTM
 from keras.layers import Dense
 from keras.layers import Activation
 from keras.layers import Dropout
-from keras.callbacks import ModelCheckpoint
 
 model = Sequential()
 model.add(Masking(mask_value=MASK_VALUE, input_shape=(X_train.shape[1:])))
-#model.add(LSTM(128, return_sequences=True))
+#model.add(LSTM(128, return_sequences=True))/
 model.add(LSTM(256, return_sequences=True))
-model.add(Dropout(0.5))
-model.add(LSTM(128))
-model.add(Dropout(0.5))
-model.add(Dense(32, activation='relu'))
+#model.add(Dropout(0.5))
+model.add(LSTM(256))
+#model.add(Dropout(0.5))
+#model.add(Dense(32, activation='relu'))
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
 #%%
+import time
+from keras.callbacks import ModelCheckpoint, TensorBoard
+import tfcallback
 from keras.optimizers import Adam
-save_path = 'checkpoints/7'
-save_prefix = 'lstm-dropout-dense-derivative-reverse'
+
+save_path = 'checkpoints/9'
+save_prefix = 'lstm-overfit'
+tensorboard_path = "log"
+
+BATCH_SIZE = 32
 
 import os
 if not os.path.exists(save_path):
@@ -60,13 +67,27 @@ if not os.path.exists(save_path):
 save_filename = save_path + "/" + save_prefix + "-{epoch:02d}-{val_categorical_accuracy:.2f}.hdf5"
 checkpointer = ModelCheckpoint(save_filename, monitor='val_categorical_accuracy', verbose=1, save_best_only=True, mode='max')
 
+tensorboard_dir = tensorboard_path + "/{}".format(time.time())
+tensorboard_callback = tfcallback.TB(log_every=1, log_dir=tensorboard_dir, write_graph=False)
+tensorboard_callback.write_batch_performance = True
+
 optimizer = Adam()
 
 model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['categorical_accuracy'])
 
 print(model.summary())
 
-model.fit(x=X_train, y=Y_train, epochs=30, verbose=1, callbacks=[checkpointer], validation_data=(X_valid, Y_valid))
+fit_params = {
+        'x': X_train,
+        'y': Y_train,
+        'epochs': 30,
+        'verbose': 1,
+        'callbacks': [checkpointer, tensorboard_callback],
+        'validation_data': (X_valid, Y_valid),
+        'batch_size': BATCH_SIZE
+}
+model.fit(**fit_params)
+#model.fit(x=X_train, y=Y_train, epochs=30, verbose=1, callbacks=[checkpointer, tensorboard_callback], validation_data=(X_valid, Y_valid), batch_size=BATCH_SIZE)
 
 test_score = tuple(model.evaluate(X_test, Y_test))
 print("Test Loss: %.3f, Test Acc: %.3f%%" % (test_score[0], test_score[1] * 100))
---
+++ tfcallback.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  7 15:21:11 2018
+
+@author: ronnymajani
+"""
+import keras.callbacks as callbacks
+import tensorflow as tf
+
+class TB(callbacks.TensorBoard):
+    def __init__(self, log_every=1, **kwargs):
+        super().__init__(**kwargs)
+        self.log_every = log_every
+        self.counter = 0
+    
+    def on_batch_end(self, batch, logs=None):
+        self.counter+=1
+        if self.counter%self.log_every==0:
+            for name, value in logs.items():
+                if name in ['batch', 'size']:
+                    continue
+                summary = tf.Summary()
+                summary_value = summary.value.add()
+                summary_value.simple_value = value.item()
+                summary_value.tag = name
+                self.writer.add_summary(summary, self.counter)
+            self.writer.flush()
+        
+        super().on_batch_end(batch, logs)
