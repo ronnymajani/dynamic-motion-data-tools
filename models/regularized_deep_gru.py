7ce8b03e7b919022cf6dfe6ab11a99eb20d00401
Ronny Majani <ronnymajani@gmail.com>
2018-04-22 17:50:26 +0300

    New mode: 512 GRU x 2 + Dense ReLU 128

---
+++ models/regularized_deep_gru.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 15:04:33 2018
+
+@author: ronnymajani
+"""
+
+
+from keras import Sequential
+from keras.layers import GRU
+from keras.layers import Dense
+from keras.layers import Activation
+from keras.layers import Dropout
+from keras.optimizers import Nadam
+from models.model_template import ModelTemplate
+
+
+class NaiveRegularizedDeepGRU(ModelTemplate):
+    NAME = "Naive Regularized Deep GRU"
+    PREFIX = "naive_regularized_deep_gru"
+    
+    def __init__(self, input_shape, **kwargs):
+        ModelTemplate.__init__(self, input_shape, **kwargs)
+        self.name = self.__class__.NAME
+        self.prefix = self.__class__.PREFIX
+    
+    def _build(self):
+        # Model
+        self.model = Sequential()
+        self.model.add(GRU(512, return_sequences=True, input_shape=self.input_shape))
+        self.model.add(Dropout(0.5))
+        self.model.add(GRU(512))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(128))
+        self.model.add(Activation('relu'))
+        self.model.add(Dense(10))
+        self.model.add(Activation('softmax'))
+        # Optimizer
+        self.optimizer = Nadam()
+        # Compile Model
+        self.model.compile(loss='categorical_crossentropy', optimizer=self.optimizer, metrics=['categorical_accuracy'])
+        
+    def train(self, X_train, Y_train, X_valid, Y_valid):
+        # Train Model
+        self.fit_params = {
+                'x': X_train,
+                'y': Y_train,
+                'epochs': self.num_epochs,
+                'verbose': 1,
+                'callbacks': self.callbacks,
+                'validation_data': (X_valid, Y_valid),
+                'batch_size': self.batch_size
+        }
+        self.model.fit(**self.fit_params)
+        
+    
+    
+    
+        
+
+    
+    
